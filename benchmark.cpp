fd6c86c9ff4f0c63f3867c43a94ee04cdcd3d831
Gustavo dos Santos <gustavobarbosavs@gmail.com>
2024-04-23 01:42:06 +0100

    Add files via upload

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,46 +1,18 @@
 #include "benchmark.h"
-#include <fstream>
-#include <format>
 
-void writeBenchmark(const Timer& benchmark) {
-    //The reason we do it here instead of when we are pushing the data to the vector
-    //is so we minimize the bloat inside our Timer Class
-    if (benchmark.timePerCall.size() == 0) return;
-
-    unsigned long long int sumOfCallTimes = 0;
-
-    for (unsigned long long int i = 1; i < benchmark.timePerCall.size() - 1; i++)
-        sumOfCallTimes += benchmark.timePerCall[i];
-
-    std::ofstream saveData(std::format("{}.md",benchmark.textFileName),std::fstream::out);
-    if (saveData.is_open()) {
-
-        saveData << std::format("#Function: {}\n\n",benchmark.textFileName);
-        saveData << "## Average call time: " << sumOfCallTimes / benchmark.timePerCall.size() << benchmark.units << '\n' << '\n';
-        saveData << "## Calls to function: " << benchmark.timePerCall.size() << '\n' << '\n';
-
-        for (unsigned long long int i = 0; i < benchmark.timePerCall.size(); i++)
-            saveData << benchmark.timePerCall[i] << benchmark.units << '\n' << '\n';
-
-        saveData.close();
-    }
-}
-
-Timer::Timer(int ratio,std::string functionName,size_t iterations)
+Timer::Timer(const int ratio,const std::string functionName,const size_t iterations)
 : timeMeasure(ratio),textFileName(functionName){
 
     switch (ratio) {
-    case SECONDS: units = " s"; break;
-    case MILLISECONDS: units = " ms"; break;
-    case MICROSECONDS: units = " us"; break;
-    case NANOSECONDS: units = " ns"; break;
+        case SECONDS: units[1] = 's',multiplier = 0.000000001f; break;
+        case MILLISECONDS: units[1] = 'm',units[2] = 's',multiplier = 0.000001f; break;
+        case MICROSECONDS: units[1] = 'u',units[2] = 's',multiplier = 0.000001f; break;
+        case NANOSECONDS: units[1] = 'n',units[2] = 's',multiplier = 0.001f; break;
 
-    default:
-        timeMeasure = 4;
-        units = " ns";
-        break;
-    }
-
+        default:
+            units[1] = 'n',units[2] = 's';
+            break;
+        }
     timePerCall.reserve(iterations+1);
 
 }
@@ -49,24 +21,12 @@ void Timer::Start(){
     m_StartTimepoint = std::chrono::high_resolution_clock::now();
 }
 void Timer::Stop() {
-    auto endTimePoint = std::chrono::high_resolution_clock::now();
+    const auto endTimePoint = std::chrono::high_resolution_clock::now();
 
     auto start = std::chrono::time_point_cast<std::chrono::nanoseconds>(m_StartTimepoint).time_since_epoch().count();
     auto end = std::chrono::time_point_cast<std::chrono::nanoseconds>(endTimePoint).time_since_epoch().count();
 
-    unsigned long long duration = end - start;
-
-    switch (timeMeasure) {
-    case 1:
-        duration *= 0.000000001;
-        break;
-    case 2:
-        duration *= 0.000001;
-        break;
-    case 3:
-        duration *= 0.001;
-        break;
-    }
+    unsigned long long duration = (end - start)*multiplier;
 
     timePerCall.push_back(duration);
 
--- benchmark.h
+++ benchmark.h
@@ -2,37 +2,58 @@
 #include <chrono>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <format>
+#include <numeric>
 
-#define SECONDS 1
-#define MILLISECONDS 2
-#define MICROSECONDS 3
-#define NANOSECONDS 4
+constexpr const unsigned short int SECONDS = 1;
+constexpr const unsigned short int MILLISECONDS = 2;
+constexpr const unsigned short int MICROSECONDS = 3;
+constexpr const unsigned short int NANOSECONDS = 4;
 
 class Timer {
 public:
 
     Timer() = delete;
     
-    Timer(int ratio,std::string functionName,size_t iterations);
+    Timer(const int ratio,const std::string functionName,const size_t iterations);
 
     void Start();
     void Stop();
 
-    std::string units;
+    char units[4]{};
     std::vector<unsigned long long int> timePerCall{};
     std::string textFileName;
 
 
 private:
     std::chrono::time_point< std::chrono::high_resolution_clock> m_StartTimepoint;
-    unsigned long long int timeMeasure;
+    unsigned short int timeMeasure = 4;
     unsigned long long int maxTime = 0;
+    float multiplier = 1;
 
 };
 
-void writeBenchmark(const Timer& benchmark);
+void constexpr writeBenchmark(const Timer& benchmark) {
+    //The reason we do it here instead of when we are pushing the data to the vector
+    //is so we minimize the bloat inside our Timer Class
+    if (benchmark.timePerCall.size() == 0) return;
 
+    unsigned long long int sumOfCallTimes = std::accumulate(benchmark.timePerCall.begin(),benchmark.timePerCall.end(),0);
 
+    std::ofstream saveData(std::format("{}.md",benchmark.textFileName),std::fstream::out);
+    if (saveData.is_open()) {
+
+        saveData << "#Function :" << benchmark.textFileName << "\n\n";
+        saveData << "## Average call time: " << sumOfCallTimes / benchmark.timePerCall.size() << benchmark.units << "\n\n";
+        saveData << "## Calls to function: " << benchmark.timePerCall.size() << "\n\n";
+
+        for(const unsigned long long& callTime : benchmark.timePerCall)
+            saveData << callTime << benchmark.units << "\n\n";
+
+        saveData.close();
+    }
+}
 
 /// @brief Makes x iterations of the function and writes all call times to a .md file
 /// @param ratio 
@@ -44,7 +65,7 @@ void writeBenchmark(const Timer& benchmark);
 /// @param Iterations Number of iterations
 /// @param Function The function you wish to benchmark
 template <typename Ratio,typename FunctionName,typename Iterations,typename Function, class ... Args>
-auto runStandardBenchmark(const Ratio ratio,const FunctionName functionName,Iterations iterations,const Function f,Args...args){
+void runStandardBenchmark(const Ratio ratio,const FunctionName functionName,Iterations iterations,const Function f,Args...args){
     Timer benchmark(ratio,functionName,iterations);
 
     while(iterations-- > 0){
