aec7909920dc8f023194f180bc93ba5fe17efe42
Gustavo dos Santos <gustavobarbosavs@gmail.com>
2024-04-23 14:49:17 +0100

    Update benchmark.cpp

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,18 +1,22 @@
 #include "benchmark.h"
+#include <assert.h>
+
+#define ASSERT(message,condition) assert(((void)message,condition))
 
 Timer::Timer(const int ratio,const std::string functionName,const size_t iterations)
 : timeMeasure(ratio),textFileName(functionName){
 
+    ASSERT("Iterations must have a value higher than 0",iterations > 0);
+    
     switch (ratio) {
         case SECONDS: units[1] = 's',multiplier = 0.000000001f; break;
         case MILLISECONDS: units[1] = 'm',units[2] = 's',multiplier = 0.000001f; break;
         case MICROSECONDS: units[1] = 'u',units[2] = 's',multiplier = 0.000001f; break;
         case NANOSECONDS: units[1] = 'n',units[2] = 's',multiplier = 0.001f; break;
 
-        default:
-            units[1] = 'n',units[2] = 's';
-            break;
-        }
+        default: units[1] = 'n',units[2] = 's'; break;
+
+    }
     timePerCall.reserve(iterations+1);
 
 }
